58121ac34cd5f4b2ea8db43d1c7c590db5283c16
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2019-06-04 15:19:34 -0300

    config

--- src/componentes/Corpo.js
+++ src/componentes/Corpo.js
@@ -46,7 +46,7 @@ class Corpo extends Component {
     
     axios({
       method: 'post',
-      url: "https://backend-icc.herokuapp.com/salvaValores",
+      url: "https://localhost:" + (process.env.PORT || 9000) + "/salvaValores",
       headers: { "Content-Type": "application/json" },
       data: {
         values: this.state.values,
